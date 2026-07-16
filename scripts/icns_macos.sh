1482ad259d38c35d1acc86f9767e6a4d69de647a
RichyKunBv <reepito03@gmail.com>
2026-07-15 20:36:11 -0600

    prueba iconos macos

---
+++ scripts/icns_macos.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+mkdir ~/Desktop/MiIcono.iconset 
+sips -z 16 16 ~/Desktop/original.png --out ~/Desktop/MiIcono.iconset/icon_16x16.png 
+sips -z 32 32 ~/Desktop/original.png --out ~/Desktop/MiIcono.iconset/icon_16x16@2x.png 
+sips -z 32 32 ~/Desktop/original.png --out ~/Desktop/MiIcono.iconset/icon_32x32.png 
+sips -z 64 64 ~/Desktop/original.png --out ~/Desktop/MiIcono.iconset/icon_32x32@2x.png 
+sips -z 128 128 ~/Desktop/original.png --out ~/Desktop/MiIcono.iconset/icon_128x128.png 
+sips -z 256 256 ~/Desktop/original.png --out ~/Desktop/MiIcono.iconset/icon_128x128@2x.png 
+sips -z 256 256 ~/Desktop/original.png --out ~/Desktop/MiIcono.iconset/icon_256x256.png 
+sips -z 512 512 ~/Desktop/original.png --out ~/Desktop/MiIcono.iconset/icon_256x256@2x.png 
+sips -z 512 512 ~/Desktop/original.png --out ~/Desktop/MiIcono.iconset/icon_512x512.png 
+sips -z 1024 1024 ~/Desktop/original.png --out ~/Desktop/MiIcono.iconset/icon_512x512@2x.png 
+iconutil -c icns ~/Desktop/MiIcono.iconset 
+rm -R ~/Desktop/MiIcono.iconset
